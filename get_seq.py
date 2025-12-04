6e3d3654819889b34f4d05af1a874e0fd43041cf
aniello-divaio <aniello.divaio@studio.unibo.it>
2025-12-04 01:07:43 +0100

    changes

--- get_seq.py
+++ get_seq.py
@@ -1,47 +1,24 @@
-"""
-get_seq.py
+#!/usr/bin/python
+import sys
 
-This script extracts specific protein sequences from a UniProt-formatted FASTA file, 
-based on a list of UniProt accession IDs provided in a separate text file.
+def get_ids(idlist):
+	f=open(idlist)
+	return f.read().strip().split('\n')
 
-The output is a FASTA file named like the ID file, but with '.fasta' extension.
 
-Usage:
-    python get_seq.py <id_list.txt> <input_fasta.fasta>
-"""
+def get_seq(pidlist,seqfile,p=1):
+	f=open(seqfile)
+	s=0
+	for line in f:
+		if line.startswith('>'):
+			pid=line.split('|')[p].strip()
+			s=0
+			if pid in pidlist: s=1
+		if s==1: print(line.strip())
+		
 
-from sys import argv
-from Bio import SeqIO
-import os
-
-def get_ids(file):
-    with open(file, "r") as f:
-        return {line.strip() for line in f if line.strip()}
-
-def get_seq(ids_to_get, input_fasta, output_fasta):
-    with open(input_fasta, "r") as infile, open(output_fasta, "w") as outfile:
-        for record in SeqIO.parse(infile, "fasta"):
-            try:
-                uniprot_id = record.id.split('|')[1] if '|' in record.id else record.id
-            except IndexError:
-                continue
-            if uniprot_id in ids_to_get:
-                SeqIO.write(record, outfile, "fasta")
-
-if __name__ == "__main__":
-    if len(argv) != 3:
-        print("ERROR: This program takes an ID file and a FASTA file as arguments.")
-        print("Usage: python get_seq.py <id_list.txt> <input_fasta.fasta>")
-        exit(1)
-    else:
-        id_file = argv[1]
-        input_fasta = argv[2]
-
-        # Output will be the same as the ID file but with '.fasta' extension
-        base_name = os.path.splitext(id_file)[0]
-        output_fasta = f"{base_name}.fasta"
-
-        ids = get_ids(id_file)
-        get_seq(ids, input_fasta, output_fasta)
-
-        print(f"Written output to: {output_fasta}")
+if __name__ =='__main__':
+	idlist=sys.argv[1]
+	seqfile=sys.argv[2]
+	pidlist=get_ids(idlist)
+	get_seq(pidlist,seqfile)
--- performance.py
+++ performance.py
@@ -1,104 +1,51 @@
-from sys import argv
+#!/usr/bin/python3
+
+import sys
 import math
 
-'''
-performance.py
 
-This script computes the confusion matrix, MCC, tpr, ppv. It takes as input a
-.class file and a treshold value as arguments.
+def get_cm(filename,th=1.0e-3,pe=2,pr=1):
+	cm=[[0,0],[0,0]]
+	f=open(filename)
+	for line in f:
+		v=line.rstrip().split()
+		evalue=float(v[pe])
+		r=int(v[pr])
+		if evalue <= th:
+			p=1       #pridiction
+		else:
+			p=0
+		cm[p][r]=cm[p][r]+1
+	return cm
 
-A third parameter can be set if performance evaluation of full-sequence, single domain or both is desired:
-1 -> full-sequence
-2 -> single domain
-0 or undefined -> both
 
-Usage:
-    python performance.py <set_#.class> <1e-$i>
-'''
-
-# Get the confusion matrix
-def get_cm(filename, threshold, pe, pr=1):
-    cm = [[0, 0], [0, 0]]
-    f = open(filename)
-    for line in f:
-        v = line.rstrip().split()
-        # v[pe] is the E-value, v[pr] is the true class (1 = Kunitz, 0 = non-Kunitz)
-        evalue = float(v[pe])
-        r = int(v[pr])
-        # Predict class based on threshold: below = Kunitz (1), above = non-Kunitz (0)
-        if evalue <= threshold:
-            p = 1
-        else:
-            p = 0
-        # Fill confusion matrix (prediction, real class)
-        cm[p][r] = cm[p][r] + 1
-    return cm
-
-# Compute accuracy
 def get_q2(cm):
-    n = float(cm[0][0] + cm[0][1] + cm[1][0] + cm[1][1])
-    return (cm[0][0] + cm[1][1])/n
+	n=float(cm[0][0]+cm[1][1]+cm[1][0]+cm[0][1])
+	return(cm[0][0]+cm[1][1])/n
 
-# Compute Matthews Correlation Coefficient
+
 def get_mcc(cm):
-    d = math.sqrt(
-        (cm[0][0] + cm[1][0]) *
-        (cm[0][0] + cm[0][1]) *
-        (cm[1][1] + cm[1][0]) *
-        (cm[1][1] + cm[0][1])
-    )
-    return (cm[0][0]*cm[1][1] - cm[0][1]*cm[1][0])/d
+	d=math.sqrt((cm[0][0]+cm[1][0])*(cm[0][0]+cm[0][1])*(cm[1][1]+cm[1][0])*(cm[1][1]+cm[0][1]))
+	return (cm[0][0]*cm[1][1]-cm[0][1]*cm[1][0])/d
 
-# True positive rate
 def get_tpr(cm):
-    return float(cm[1][1]) / (cm[1][0] + cm[1][1])
+	return float(cm[1][1])/(cm[1][0]+cm[1][1])
 
-# Positive predictive value (precision)
 def get_ppv(cm):
-    return float(cm[1][1]) / (cm[1][1] + cm[0][1])
+	return float(cm[1][1])/(cm[1][1]+cm[0][1])
 
-def full_seq_computing(filename, th):
-    cm = get_cm(filename, th, 2)
-    print("USING E-VALUE OF THE FULL SEQUENCE...")
-    q2 = get_q2(cm)
-    print("tn=", cm[0][0], "fn=", cm[0][1])  # true negatives, false negatives
-    print("fp=", cm[1][0], "tp=", cm[1][1])  # false positives, true positives
-    mcc = get_mcc(cm)
-    tpr = get_tpr(cm)
-    ppv = get_ppv(cm)
-    print("threshold=", th, "q2=", q2, "MCC=", mcc, "tpr=", tpr, "ppv=", ppv, "fullseq=", True)
-    return
+if __name__ == '__main__':
 
-def single_domain_computing(filename, th):
-    cm = get_cm(filename, th, 3)
-    print("USING E-VALUE OF THE BEST DOMAIN...")
-    q2 = get_q2(cm)
-    print("tn=", cm[0][0], "fn=", cm[0][1])  # true negatives, false negatives
-    print("fp=", cm[1][0], "tp=", cm[1][1])  # false positives, true positives
-    mcc = get_mcc(cm)
-    tpr = get_tpr(cm)
-    ppv = get_ppv(cm)
-    print("threshold=", th, "q2=", q2, "MCC=", mcc, "tpr=", tpr, "ppv=", ppv, "fullseq=", False)
-    return
-
-if __name__ == "__main__":
-    filename = argv[1]
-    th = float(argv[2])
-    if len(argv) > 3:
-        selection = int(argv[3])
-        # selection = 1 → evaluate only full sequence
-        # selection = 2 → evaluate only best domain
-        # selection = 0 or undefined → evaluate both
-    else:
-        selection = 0
-    if selection == 0:
-        # Evaluate both full sequence and best domain using the same threshold
-        full_seq_computing(filename, th)
-        print("\n")
-        single_domain_computing(filename, th)
-    elif selection == 1:
-        # Evaluate only full sequence
-        full_seq_computing(filename, th)
-    else:
-        # Evaluate only best domain
-        single_domain_computing(filename, th)
+	filename=sys.argv[1]
+	th=float(sys.argv[2])
+	cm=get_cm(filename,th)
+	q2=get_q2(cm)
+	mcc=get_mcc(cm)
+	tpr=get_tpr(cm)
+	ppv=get_ppv(cm)
+	print('TN=' ,cm[0][0], 'FN=' ,cm[0][1])
+	print('FP=' ,cm[1][0], 'TP=' ,cm[1][1])
+	print('TH=', th, 'Q2=' ,q2, 'MCC=' ,mcc, 'TPR=' , tpr, 'PPV=', ppv)
+	
+	
+	
